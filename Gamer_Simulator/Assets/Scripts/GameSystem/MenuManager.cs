6dca721dce3ef675ce51008a907de3c0db78700f
AntonioSantos8 <afsantos082008@gmail.com>
2025-05-18 17:30:46 -0300

    Menu made

---
+++ Gamer_Simulator/Assets/Scripts/GameSystem/MenuManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Simulator");
+    }
+}
